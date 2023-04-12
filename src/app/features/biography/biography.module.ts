5201e9d74488afb99550e764c7ee214a5c0d768e
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-04-12 11:58:55 -0400

    added projects component

--- src/app/features/biography/biography.module.ts
+++ src/app/features/biography/biography.module.ts
@@ -6,13 +6,14 @@ import { BiographyComponent } from './biography.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutComponent } from '../about/about.component';
 import { IconCarrouselComponent } from '../icon-carrousel/icon-carrousel.component';
-
+import { ProjectsComponent } from '../projects/projects.component';
 
 @NgModule({
   declarations: [
     BiographyComponent,
     AboutComponent,
-    IconCarrouselComponent
+    IconCarrouselComponent,
+    ProjectsComponent
   ],
   imports: [
     CommonModule,
