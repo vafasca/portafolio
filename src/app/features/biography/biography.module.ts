a7ab9bf8fce019f8fc70cacf20b07aaddc335028
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-04-12 00:45:49 -0400

    added images, carrousel-icon

--- src/app/features/biography/biography.module.ts
+++ src/app/features/biography/biography.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { BiographyRoutingModule } from './biography-routing.module';
 import { BiographyComponent } from './biography.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AboutComponent } from '../about/about.component';
+import { IconCarrouselComponent } from '../icon-carrousel/icon-carrousel.component';
 
 
 @NgModule({
   declarations: [
-    BiographyComponent
+    BiographyComponent,
+    AboutComponent,
+    IconCarrouselComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/features/icon-carrousel/icon-carrousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-carrousel',
+  templateUrl: './icon-carrousel.component.html',
+  styleUrls: ['./icon-carrousel.component.css']
+})
+export class IconCarrouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
